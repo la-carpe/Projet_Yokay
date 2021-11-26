404b004316678fc42811ac495e205907487ee395
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2021-11-26 16:23:31 +0100

    a

--- src/Position.java
+++ src/Position.java
@@ -1,2 +1,29 @@
 public class Position {
+
+    private char column;
+    private int row;
+    public Position(char column, int row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+
+    // Conversion vers une abscisse StdDraw :
+    // - si column vaut 'a' alors (column - 'a') vaut 0
+    // - si column vaut 'b' alors (column - 'a') vaut 1
+    // - si column vaut 'c' alors (column - 'a') vaut 2
+    // - ...
+    public int getX() {
+        return this.column - 'a';
+    }
+
+    // Conversion vers une ordonnée StdDraw :
+    public int getY() {
+        return this.row - 1;
+    }
 }
+
