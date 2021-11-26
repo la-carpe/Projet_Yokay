6a84611361dc2fb99a6e2f95b1e4136b7ddae882
la-carpe <64590614+la-carpe@users.noreply.github.com>
2021-11-26 15:56:26 +0100

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name +
+                '}';
+    }
+}
